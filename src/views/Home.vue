93ef76c71065bc37fb7c939ac4d061c8bee537fe
carina092 <carina@carinascheiberlich.nl>
2019-06-09 22:30:46 +0200

    Empty Vue project.

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+// import x from '@/components/x.vue';
+
+export default {
+  name: 'home',
+  components: {
+  },
+};
+</script>
